e5f25ad448fc543af432f108c75458f405713051
Ivailo Ignatov <ivailo.ignatoff@gmail.com>
2021-04-13 00:39:44 +0300

    refactor iban formatter

--- C01/C01P01 - IBAN formatter/solution.py
+++ C01/C01P01 - IBAN formatter/solution.py
@@ -16,14 +16,14 @@ def get_time(func):
 @get_time
 def iban_formatter_string_concatenator(iban):
     iban = iban.replace(' ', '')
-    formated = ''
+    formatted = ''
 
     for index, char in enumerate(iban):
         if index % 4 == 0 and index > 0:
-            formated += ' '
-        formated += char
+            formatted += ' '
+        formatted += char
 
-    return formated
+    return formatted
 
 
 @get_time
@@ -64,16 +64,16 @@ formatters = (
 
 tests = (
     ("BG91UNCR70001864961754", "BG91 UNCR 7000 1864 9617 54"),
-    # ("BG80 BNBG 9661 1020 3456 78", "BG80 BNBG 9661 1020 3456 78"),
-    # ("BG14TTBB94005362446381", "BG14 TTBB 9400 5362 4463 81"),
-    # ("BG80BNBG96611020345678", "BG80 BNBG 9661 1020 3456 78")
+    ("BG80 BNBG 9661 1020 3456 78", "BG80 BNBG 9661 1020 3456 78"),
+    ("BG14TTBB94005362446381", "BG14 TTBB 9400 5362 4463 81"),
+    ("BG80BNBG96611020345678", "BG80 BNBG 9661 1020 3456 78")
 )
 
-# for func in formatters:
-#     for iban, expected in tests:
-#         result, exec_time, info = func(iban)
-#         print(info)
-#         print(f'{expected} == {result}')
-#         print(expected == result)
-#         print(exec_time)
-#         print()
+for func in formatters:
+    for iban, expected in tests:
+        result, exec_time, info = func(iban)
+        print(info)
+        print(f'{expected} == {result}')
+        print(expected == result)
+        print(exec_time)
+        print()
