e10933c669512850a90c5565c976c74aed1e6cf0
ivo-bass <ivailo.ignatoff@gmail.com>
2021-05-02 23:19:11 +0300

    refactor matrix bombing solution

--- C01/C01P18 - Matrix bombing /solution.py
+++ C01/C01P18 - Matrix bombing /solution.py
@@ -10,8 +10,8 @@ DELTAS = (
 )
 
 
-def is_valid_index(index, matrix):
-    return 0 <= index < len(matrix)
+def is_valid_index(index, length):
+    return 0 <= index < length
 
 
 def sum_matrix(m):
@@ -20,20 +20,26 @@ def sum_matrix(m):
 
 def bomb_neighbours(matrix, x, y):
     value = matrix[x][y]
+
     for delta_x, delta_y in DELTAS:
         row_index = x + delta_x
         col_index = y + delta_y
-        if not is_valid_index(row_index, matrix) or not is_valid_index(col_index, matrix):
+
+        if not is_valid_index(row_index, len(matrix)) \
+                or not is_valid_index(col_index, len(matrix[0])):
             continue
+
         current_value = matrix[row_index][col_index]
         matrix[row_index][col_index] = current_value - value if current_value >= value else 0
 
 
 def matrix_bombing_plan(matrix):
     result_dict = {}
+
     for r in range(len(matrix)):
         for c in range(len(matrix[0])):
             matrix_copy = [row.copy() for row in matrix]
             bomb_neighbours(matrix_copy, r, c)
             result_dict[(r, c)] = sum_matrix(matrix_copy)
+
     return result_dict
