7e0ae96cc11ba7fcc09a0b55ca8f42814b71aa93
ivo-bass <ivailo.ignatoff@gmail.com>
2021-05-30 12:36:01 +0300

    refactor solution 3

--- C01/C01P03 - Factorial digits/solution.py
+++ C01/C01P03 - Factorial digits/solution.py
@@ -1,14 +1,25 @@
 def fact_digits(n):
-    return sum(factorial(int(s)) for s in str(abs(n)))
+    # return sum(factorial(int(ch)) for ch in str(abs(n)))
+
+    n = abs(n)
+
+    result = 0
+    while n != 0:
+        digit = n % 10
+        result += factorial(digit)
+        n = n // 10
+
+    return result
+
 
 def factorial(n):
     fact = 1
-    for i in range(1, n+1): 
+    for i in range(1, n + 1):
         fact *= i
     return fact
 
 
-print(fact_digits(101))
-print(fact_digits(111))
-print(fact_digits(145))
-print(fact_digits(999))
+print(fact_digits(101) == 3)
+print(fact_digits(111) == 3)
+print(fact_digits(145) == 145)
+print(fact_digits(999) == 1088640)
