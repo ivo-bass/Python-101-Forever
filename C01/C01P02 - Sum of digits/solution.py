59eec573979b87dbee701e9cb1388c69c1773e98
ivo-bass <ivailo.ignatoff@gmail.com>
2021-05-30 12:25:54 +0300

    refactor solution 2

--- C01/C01P02 - Sum of digits/solution.py
+++ C01/C01P02 - Sum of digits/solution.py
@@ -1,8 +1,17 @@
 def sum_of_digits(n):
-    return sum(int(s) for s in str(abs(n)))
+    # return sum(int(ch) for ch in str(abs(n)))
+
+    absolute_n = abs(n)
+    string_n = str(absolute_n)
+
+    result = 0
+    for char in string_n:
+        result += int(char)
+
+    return result
 
 
-print(sum_of_digits(1325132435356))
-print(sum_of_digits(123))
-print(sum_of_digits(6))
-print(sum_of_digits(10))
+print(sum_of_digits(1325132435356) == 43)
+print(sum_of_digits(123) == 6)
+print(sum_of_digits(6) == 6)
+print(sum_of_digits(10) == 1)
