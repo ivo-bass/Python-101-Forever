ef62a76b10aa43cc67fc5e0808586e7c19bdd2c3
ivo-bass <ivailo.ignatoff@gmail.com>
2021-05-30 12:48:09 +0300

    refactor solution 5

--- C01/C01P05 - Sum matrix /solution.py
+++ C01/C01P05 - Sum matrix /solution.py
@@ -1,5 +1,13 @@
 def sum_matrix(matrix):
-    return sum(sum(matrix[row_i]) for row_i in range(len(matrix)))
+    # return sum(sum(matrix[row_i]) for row_i in range(len(matrix)))
+
+    result = 0
+
+    for row in matrix:
+        for element in row:
+            result += element
+
+    return result
 
 
 m = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
