1dfb61ed51ebb398df2a46f3ec1a002cd17b666c
ivo-bass <ivailo.ignatoff@gmail.com>
2021-05-30 13:47:52 +0300

    refastor solution 10

--- C01/C01P10 - Palindromes count /solution.py
+++ C01/C01P10 - Palindromes count /solution.py
@@ -1,9 +1,23 @@
+def is_palindrome(number):
+    num_list = [s for s in str(number)]
+    reversed_list = list(reversed(num_list))
+    return num_list == reversed_list
+
+
+def build_cache():
+    dd = {}
+    for number in range(11, 100000):
+        dd[number] = is_palindrome(number)
+    return dd
+
+
+CACHE = build_cache()
+
+
 def palindromes_count(n):
     count = 0
-    for number in range(10, n+1):
-        num_list = [s for s in str(number)]
-        reversed_list = list(reversed(num_list))
-        if num_list == reversed_list:
+    for number in range(11, n+1):
+        if CACHE[number]:
             count += 1
     return count
 
@@ -11,5 +25,9 @@ def palindromes_count(n):
 print(palindromes_count(10) == 0)
 print(palindromes_count(20) == 1)  # only 11
 print(palindromes_count(101) == 10)  # 11, 22, 33, 44, 55, 66, 77, 88, 99, 101
+print(palindromes_count(359) == 35)
+print(palindromes_count(1698) == 106)
+print(palindromes_count(5098) == 140)
+print(palindromes_count(10001) == 190)
 print(palindromes_count(92009) == 1009)
 print(palindromes_count(99999) == 1089)
