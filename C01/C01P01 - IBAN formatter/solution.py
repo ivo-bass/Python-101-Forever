44e212cea689bea0ef4701e5d9181d8ad5ad263e
ivo-bass <ivailo.ignatoff@gmail.com>
2021-05-30 12:17:30 +0300

    rewrite solution 1 remove .replace()

--- C01/C01P01 - IBAN formatter/solution.py
+++ C01/C01P01 - IBAN formatter/solution.py
@@ -1,79 +1,39 @@
-from time import time
+SPACE = ' '
 
 
-def get_time(func):
-    def wrapper(iban):
-        start = time()
-        result = func(iban)
-        end = time()
-        exec_time = end - start
-        info = func.__name__
-        return result, exec_time, info
+def iban_formatter(iban):
+    result = []
+    counter = 0
 
-    return wrapper
+    for char in iban:
 
+        if char == SPACE:
+            continue
 
-@get_time
-def iban_formatter_string_concatenator(iban):
-    iban = iban.replace(' ', '')
-    formatted = ''
+        if counter == 4:
+            result.append(SPACE)
+            counter = 0
 
-    for index, char in enumerate(iban):
-        if index % 4 == 0 and index > 0:
-            formatted += ' '
-        formatted += char
+        result.append(char)
+        counter += 1
 
-    return formatted
+    return ''.join(result)
 
 
-@get_time
-def iban_formatter_append_to_list(iban):
-    iban = iban.replace(' ', '')
-    ll = []
 
-    for index, char in enumerate(iban):
-        if index % 4 == 0 and index > 0:
-            ll.append(' ')
-        ll.append(char)
+def test():
+    tests = (
+        ("BG91UNCR70001864961754", "BG91 UNCR 7000 1864 9617 54"),
+        ("BG80 BNBG 9661 1020 3456 78", "BG80 BNBG 9661 1020 3456 78"),
+        ("BG14TTBB94005362446381", "BG14 TTBB 9400 5362 4463 81"),
+        ("BG80BNBG96611020345678", "BG80 BNBG 9661 1020 3456 78")
+    )
 
-    return ''.join(ll)
-
-
-@get_time
-def iban_formatter_slicing_comprehension(iban):
-    iban = iban.replace(' ', '')
-    result = [iban[i:i + 4] for i in range(0, len(iban), 4)]
-    return ' '.join(result)
-
-
-@get_time
-def iban_formatter_for_loop_slicing(iban):
-    iban = iban.replace(' ', '')
-    ll = []
-    for i in range(0, len(iban), 4):
-        ll.append(iban[i:i + 4])
-    return ' '.join(ll)
-
-
-formatters = (
-    iban_formatter_string_concatenator,
-    iban_formatter_append_to_list,
-    iban_formatter_slicing_comprehension,
-    iban_formatter_for_loop_slicing
-)
-
-tests = (
-    ("BG91UNCR70001864961754", "BG91 UNCR 7000 1864 9617 54"),
-    ("BG80 BNBG 9661 1020 3456 78", "BG80 BNBG 9661 1020 3456 78"),
-    ("BG14TTBB94005362446381", "BG14 TTBB 9400 5362 4463 81"),
-    ("BG80BNBG96611020345678", "BG80 BNBG 9661 1020 3456 78")
-)
-
-for func in formatters:
     for iban, expected in tests:
-        result, exec_time, info = func(iban)
-        print(info)
-        print(f'{expected} == {result}')
+        result = iban_formatter(iban)
+        print(f'{expected} ==? {result}')
         print(expected == result)
-        print(exec_time)
         print()
+
+
+test()
